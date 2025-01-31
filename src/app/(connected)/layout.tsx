3ce0edab3537f7706c7865589446ded32d2f4a83
antho1404 <anthony.estebe@gmail.com>
2025-01-31 18:13:04 +0700

    update design

--- src/app/(connected)/layout.tsx
+++ src/app/(connected)/layout.tsx
@@ -4,11 +4,9 @@ import { PropsWithChildren } from "react";
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <>
-      <main className="space-y-6">
+      <main>
         <Header /> 
-        <div className="mx-auto max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
-          {children}
-        </div>
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">{children}</div>
       </main>
     </>
   );
